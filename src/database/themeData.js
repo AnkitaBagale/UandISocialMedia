cc3b0363d7df88b5606d17447c53ee15681b0438
ankitabagale <manali.bagale@gmail.com>
2021-06-03 00:22:17 +0530

    initial commit

---
+++ src/database/themeData.js
@@ -0,0 +1,28 @@
+export const colors = {
+	pink: {
+		900: '#e63961',
+		800: '#ff3f6c',
+		700: '#FF6589',
+		400: 'rgb(234, 234, 236,0.6)',
+		300: '#ffd9e2',
+		100: '#fff7f9',
+	},
+	gray: {
+		900: '#282c3f',
+		800: '#3f4049',
+		700: '#696b79',
+		600: '#d8d8da',
+		400: '#eaeaec',
+		300: '#f5f5f6',
+		100: 'white',
+	},
+	violet: {
+		900: '#3F3947',
+		300: '#a7a7a7',
+	},
+};
+
+export const fonts = {
+	heading: 'Roboto',
+	body: 'Roboto',
+};
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { colors, fonts } from './database';
+import { ScrollToTop } from './ScrollToTop';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const theme = extendTheme({
+	colors,
+	fonts,
+});
+
+ReactDOM.render(
+	<React.StrictMode>
+		<ChakraProvider theme={theme}>
+			<Router>
+				<Provider store={store}>
+					<App />
+					<ScrollToTop />
+				</Provider>
+			</Router>
+		</ChakraProvider>
+	</React.StrictMode>,
+	document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
